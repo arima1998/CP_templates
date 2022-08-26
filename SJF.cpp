ca1cd0c60dbf62521998b12aff422896eec1ceee
arima1998 <69233243+arima1998@users.noreply.github.com>
2022-08-26 15:13:24 +0600

    Create SJF.cpp

---
+++ SJF.cpp
@@ -0,0 +1,158 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define lld long double
+#define ul  unsigned long int
+#define pii pair<int,int>
+#define pll pair<lli, lli>
+#define vi  vector<int>
+#define vii vector<pair<int,int>>
+#define vll vector<lli>
+#define pb  push_back
+#define mpr  make_pair
+#define ss second
+#define ff first
+#define pie 3.14159265358979323846264338327950L
+#define all(x) x.begin(),x.end()
+#define testcase() int tc;cin>>tc;while(tc--)
+#define clean(x,y) memset(x,y,sizeof(x))
+#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+#define loop(i,st,ed) for(int i=st;i<=ed;i++)
+#define debug cout<<"HERE is ok"<<endl
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";}
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.ff << ", " << p.ss << ")"; }
+template<typename A>ostream& operator<<(ostream &cout , set<A>const &s){cout<<"[";for(A i:s){cout<<i<<" ";}return cout<<"]";}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+	cin >> p.first;
+	return cin >> p.second;
+}
+
+struct compare{
+    bool operator()(const pii & a, const pii & b)
+    {
+        return a.ff > b.ff;
+    }
+};
+
+void showQ(priority_queue<pii , vector<pii>,compare>q)
+{
+    while(!q.empty())
+    {
+        cout<<"("<<q.top().ff<<" "<<q.top().ss<<" )";
+        q.pop();
+    }
+    cout<<endl;
+}
+
+int main()
+{
+
+    int n;
+    cout<<"Input number of process: ";
+    cin>>n;
+    int burstTime[n+1],arrivalTime[n+1];
+    int waitingTime[n+1],turnaroundTime[n+1];
+    int startTime[n+1],endTime[n+1],responseTime[n+1],timeQuantum;
+    vector<pair<int,int>>process;
+    vector<pii>granttChart;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"Enter Arrival Time of P"<<i<<": ";
+        cin>>arrivalTime[i];
+        cout<<"Enter Burst Time of P"<<i<<": ";
+        cin>>burstTime[i];
+        process.push_back({arrivalTime[i],i});
+        startTime[i]=INT_MAX;
+    }
+
+
+
+    sort(process.begin(),process.end(),[](pii a,pii b){return a.ff <= b.ff ;});
+    int curTime=0;
+    int totalidleTime=0,done=0,vis[n+1]={0};
+    priority_queue<pii , vector<pii>,compare>currentProcess;
+    cout<<process<<endl;
+
+    while(1)
+    {
+        if(done == n)break;
+        if(currentProcess.empty())
+        {
+            for(int i=0;i<n;i++)
+            {
+                int id=process[i].ss;
+                if(vis[id] == 0)
+                {
+                    vis[id]=1;
+                    currentProcess.push({burstTime[id],id});
+                    break;
+                }
+            }
+        }
+
+        pii job=currentProcess.top();
+        currentProcess.pop();
+        int id=job.ss;
+        int remTime=job.ff;
+
+        if(curTime < arrivalTime[id])
+        {
+            int diff=arrivalTime[id]-curTime;
+            totalidleTime+=diff;
+            granttChart.push_back({curTime,-1});
+        }
+        curTime=max(curTime,arrivalTime[id]);
+
+        startTime[id]=min(curTime,startTime[id]);
+        granttChart.pb({curTime,id});
+        curTime+=remTime;
+        remTime=0;
+        endTime[id]=curTime;
+        for(int k=0;k<n;k++)
+        {
+            int id_=process[k].ss;
+            if(vis[id_] == 0 and process[k].ff <= curTime)
+            {
+                vis[id_]=1;
+                currentProcess.push({burstTime[id_],id_});
+            }
+        }
+        done++;
+    }
+    cout<<"Grantt Chart"<<endl;
+    for(pii i:granttChart)
+    {
+        if(i.ss == -1)
+        {
+            cout<<i.ff<<" "<<"IDLE ";
+        }
+        else
+        {
+            cout<<i.ff<<" P"<<i.ss<<" ";
+        }
+    }
+    cout<<endl;
+    cout<<setw(6);
+    for(int i=1;i<=n;i++)
+    {
+        int tt=endTime[i]-arrivalTime[i];
+        int wt=tt-burstTime[i];
+        waitingTime[i]=wt;
+        turnaroundTime[i]=tt;
+        responseTime[i]=startTime[i]-arrivalTime[i];
+        cout<<"Process: P"<<i<<" Finish time:"<<endTime[i]<<" Response time: "<<startTime[i]-arrivalTime[i]<<" Waiting time: "<<wt<<" Turnaround time: "<<tt<<endl;
+    }
+    cout<<endl;
+    int totalwt=0,totaltt=0;
+    for(int i=1;i<=n;i++)
+    {
+        totalwt+=waitingTime[i];
+        totaltt+=turnaroundTime[i];
+    }
+    cout<<setprecision(3);
+    cout<<"Average waiting time : "<<(double)totalwt/(double)n<<endl;
+    cout<<"Average turnaround time : "<<(double)totaltt/(double)n<<endl;
+    cout<<"Idle time: "<<totalidleTime<<endl;
+
+
+}
