00e7055d610b7e1ed8d97f0973c0ae9420b02a39
arima1998 <69233243+arima1998@users.noreply.github.com>
2023-07-30 07:39:57 +0600

    Update BasicHashingForme.cpp

--- stringhahsing/usaco/BasicHashingForme.cpp
+++ stringhahsing/usaco/BasicHashingForme.cpp
@@ -1,25 +1,28 @@
 class Hashing {
     private:
-        static const long long Mod = 1e9+9;
-        static const long long base = 31;
+        static const ll Mod = (1LL << 61) - 1;
+        static const ll base;
         static vector<long long>pow;
         vector<long long>p_hash;
+        __int128 mul(ll a, ll b) { return (__int128)a * b; }
+        ll mod_mul(ll a, ll b) { return mul(a, b) % Mod; }
     public:
         Hashing(const string &s): p_hash(s.size()+1){
-            while(pow.size() < s.size()){pow.push_back((pow.back() * base)%Mod);}
+            while(pow.size() < s.size()){ pow.push_back(mod_mul(pow.back(), base)); }
             p_hash[0]=0;
             for(int i=0;i<s.size();i++)
             {
-                p_hash[i+1]=((p_hash[i]*base)%Mod + s[i])%Mod;
+                p_hash[i+1]=(mul(p_hash[i], base) + s[i])%Mod;
             }
         }
         /// I used string to be 0 indexed
         long long getHash(int st,int ed)
         {
-            long long val=(p_hash[ed+1]-(p_hash[st]*pow[ed-st+1]));
+            long long val=p_hash[ed + 1] - mod_mul(p_hash[st], pow[ed - st + 1]);
             return (val%Mod+Mod)%Mod;
         }
-        
+
 };
-/// Hashing h1(s) ..to start
-vector<long long>Hashing::pow={1};
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+vector<ll> Hashing::pow = {1};
+const ll Hashing::base = uniform_int_distribution<ll>(0, Mod - 1)(rng);
